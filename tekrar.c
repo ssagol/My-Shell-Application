26599c6999995ca4fd6ce27eaac79fea76476454
Saliha SAGOL <55808784+ssagol@users.noreply.github.com>
2021-12-12 22:58:52 +0300

    Update tekrar.c

--- tekrar.c
+++ tekrar.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
+
+int main(int argc, char *argv[]){
     // ilk girilen input işlem adı  A[0]
     // ikinci girien input ekrana ne yazılması gerektiği  A[1]
     // üçüncü girilen input ise tekrar sayısı  A[2]
 
     // tekrar Saliha 4  gibi..
-    int count = atoi(argv[2]);  //tekrar sayısı, 2 indeks
-    int temp=0;  
 
-    while (temp < count){  // kaç kez yazılacak olana kadar koşul
-        printf("%s\n", argv[1]); // ne yazılacak
-        temp++;  // kaç kez yazılacak
+	if(numCheck(argv[2])){  // önce sayı kontrolü
+		int i;
+		for(i = 0; i< atoi(argv[2]);i++){
+			printf("%s",argv[1]);  // ekrana yazılacak metni basar
+			printf("\n");
+		}
+		printf("\n");
+	}
+	else{
+        printf("Yanlis giris yapildi");
+
     }
-    return 0;
+	
+	return 0;
+}
+
+int numCheck (char *argv){  
+    int i = 0;
+	while(argv[i] != '\0'){ // karakter olmayana kadar dön
+		if(argv[i]  <= '0' || argv[i] >= '9'){ // eğer 0-9 arasında bişey değilse
+			return 0;   // num değilse 0 dönsün
+		}else{
+         	return 1;  // num ise 1 dönsün
+        }
+		i++;
+    }
 }
